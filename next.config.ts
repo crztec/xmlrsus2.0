b74daf3fe9a05278c1dba127f1d3488baa92de93
unknown <crztec@users.noreply.github.com>
2026-06-25 11:08:32 -0300

    chore: add basePath /rsus and update api rewrite

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  basePath: '/rsus', // <-- ESTA LINHA CONSERTA A RENDERIZAÇÃO NA SUBPASTA
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        // Aqui usamos a variável de ambiente. Se ela não existir, ele assume o localhost:8000 como backup
-        destination: `${process.env.API_URL || 'http://localhost:8000'}/:path*`,
+        // Agora o Next sabe que a API dele responde na subpasta de API
+        destination: 'https://crztech.com.br/api-rsus/:path*', 
       },
     ];
   },
